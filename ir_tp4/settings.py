c90b9e0a8a922c4d80412ba6f540a462d955065a
Rizky Juniastiar <rizkyjuniastiar37@gmail.com>
2023-12-05 23:44:57 +0700

    edit static root

--- ir_tp4/settings.py
+++ ir_tp4/settings.py
@@ -121,6 +121,23 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# This is the directory for storing `collectstatic` results.
+# This shouldn't be included in your Git repository.
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# You can use this directory to store project-wide static files.
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+# Make sure the directories exist to prevent errors when doing `collectstatic`.
+for directory in [*STATICFILES_DIRS, STATIC_ROOT]:
+    directory.mkdir(exist_ok=True)
+
+# Using Django StaticFilesStorage for staticfiles.
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
